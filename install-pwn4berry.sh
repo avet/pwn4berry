3d88daa6fae49abae11b9b6e7610254f1ff2f4c8
AVET INS <avet@users.noreply.github.com>
2015-09-09 14:31:00 +0200

    Some code cleanup

--- install-pwn4berry.sh
+++ install-pwn4berry.sh
@@ -7,13 +7,24 @@
 # Latest version of this code can be downloaded at: https://github.com/avet/pwn4berry
 
 user_name =$(whoami)
-echo "pwn4berry version 0.2a (c) 2014 - 2015 AVET Information and Network Security Sp. z o.o. 2014"
+pwn_verfile = /etc/pwn4berry
+pwn_ver = "0.2b"
+pwn_installbase = ./install-base
+
+echo "pwn4berry version 0.2b (c) 2014 - 2015 AVET Information and Network Security Sp. z o.o. 2014"
 echo "AVET INS toolkit for pentesters using Raspberry Pi Raspbian"
 echo "Lite version for non commercial use only. WiFi support has been removed"
-echo 
-if [ -f /etc/debian_version ] || [ user_name -ne "pi" ]; then
-	if [ -f ./install-base ]; then
-		rm -rf ./install-base
+echo
+if [ -f /etc/debian_version ] || [ $user_name -ne "pi" ]; then
+	if [ -f $pwn_verfile ]; then
+		echo "This installation will update current pwn4berry version if  possible."
+	else
+		sudo cat $pwn_ver > $pwn_verfile
+	fi
+
+	if [ -f $pwn_installbase ]; then
+		echo " - Deleting old installation base directory"
+		rm -rf $pwn_installbase
 	fi
 
 	echo " + Installing basic components"
@@ -55,4 +66,3 @@ if [ -f /etc/debian_version ] || [ user_name -ne "pi" ]; then
 else
 	echo "Error: pwn4berry can be installed only on Raspbian system using default pi account"
 fi
-
